1f77e03b7c0238532697ab5da26138b3e9e2f3b0
minkyung <mnk918p@gmail.com>
2020-04-24 17:54:44 +0900

    [ADD] main page with image banner

--- src/pages/main/Index.js
+++ src/pages/main/Index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import NavBar from "../../components/common/nav/Navbar";
+import ImgSection from "../../components/common/ImgSection";
 
 class Index extends React.Component {
   render() {
     return (
       <div>
         <NavBar />
+        <ImgSection />
         <h1>index 입니다.</h1>
       </div>
     );
