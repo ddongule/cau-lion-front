601e26d9346e53bd6a35f1399824119120faa26d
minkyung <mnk918p@gmail.com>
2020-04-24 17:30:51 +0900

    [ADD] Navbar component, design

--- src/components/common/nav/NavItem.js
+++ src/components/common/nav/NavItem.js
@@ -2,11 +2,10 @@ import React from "react";
 
 class NavItem extends React.Component {
   render() {
-    return (
-      <div>
-        <h1>NavItem 입니다.</h1>
-      </div>
-    );
+    return <div>
+      ddk
+      
+    </div>;
   }
 }
 
--- src/components/common/nav/Navbar.js
+++ src/components/common/nav/Navbar.js
@@ -1,11 +1,24 @@
 import React from "react";
+import "../../../assets/css/common/base/Base.scss";
 import "../../../assets/css/common/nav/Navbar.scss";
+import temp from "../../../assets/img/temp.png";
 
 class NavBar extends React.Component {
   render() {
     return (
-      <div className="nav">
-        <h1>Navbar 입니다.</h1>
+      <div className="main-nav">
+        <div className="main-nav-logo">
+          {/* temporary logo */}
+          <img src={temp} />
+        </div>
+        <div className="main-nav-list">
+          <ul className="main-nav-box">
+            <li className="main-nav-item">활동 일정</li>
+            <li className="main-nav-item">기술 블로그</li>
+            <li className="main-nav-item">포트폴리오</li>
+            <li className="main-nav-item">로그인</li>
+          </ul>
+        </div>
       </div>
     );
   }
