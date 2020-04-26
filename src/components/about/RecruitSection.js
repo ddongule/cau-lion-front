e28c82c7f59fedb81bcf6e0280da5f24ebffacb2
ddongule <mnk918p@gmail.com>
2020-04-26 18:10:41 +0900

    [UPDATE] Nav Item

--- src/components/about/RecruitSection.js
+++ src/components/about/RecruitSection.js
@@ -1,10 +1,12 @@
 import React from "react";
+import ImageSection from "../common/ImgSection";
 
 class RecruitSection extends React.Component {
   render() {
     return (
       <div>
-        <h1>RecruitSection이 나오고 있음</h1>
+        Section Header가 들어갈 부분입니다.
+        <ImageSection />
       </div>
     );
   }
--- src/components/common/nav/NavItem.js
+++ src/components/common/nav/NavItem.js
@@ -1,22 +1,28 @@
 import React from "react";
 
 class NavItem extends React.Component {
+  getNavItems = () => {
+    const navItem = [
+      { id: 0, item: "활동일정", link: "#" },
+      { id: 1, item: "기술블로그", link: "#" },
+      { id: 2, item: "포트폴리오", link: "#" },
+      { id: 3, item: "로그인", link: "#" },
+    ];
+    return navItem;
+  };
+  state = {
+    navItems: this.getNavItems(),
+  };
+
   render() {
     return (
       <div className="main-nav-list">
         <ul className="main-nav-box">
-          <li className="main-nav-item">
-            <a href="#">활동 일정</a>
-          </li>
-          <li className="main-nav-item">
-            <a href="#">기술 블로그</a>
-          </li>
-          <li className="main-nav-item">
-            <a href="#">포트폴리오</a>
-          </li>
-          <li className="main-nav-item">
-            <a href="#">로그인</a>
-          </li>
+          {this.state.navItems.map((nav) => (
+            <li className="main-nav-item">
+              <a href={nav.link}>{nav.item}</a>
+            </li>
+          ))}
         </ul>
       </div>
     );
