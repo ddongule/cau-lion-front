08b318969bf9942f446fadd0b23942a4fda98298
YouuInKeun <keun0390@naver.com>
2020-04-27 02:44:00 +0900

    [UPDATE] card content className

--- src/components/common/Card.js
+++ src/components/common/Card.js
@@ -5,7 +5,7 @@ class Card extends React.Component {
   render() {
     return (
       <div className="card">
-        <h1 className="card-title">{this.props.card.content}</h1>
+        <h1 className="card-content">{this.props.card.content}</h1>
         <h1 className="card-category">{this.props.card.category}</h1>
       </div>
     );
