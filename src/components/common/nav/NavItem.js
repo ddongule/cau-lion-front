bdc507d2373c5c7eb892909d9778ed0f9e03b825
ddongule <mnk918p@gmail.com>
2020-04-27 21:49:24 +0900

    [UPDATE] Navbar, NavItem props

--- src/components/common/nav/NavItem.js
+++ src/components/common/nav/NavItem.js
@@ -1,28 +1,13 @@
 import React from "react";
 
 class NavItem extends React.Component {
-  getNavItems = () => {
-    const navItem = [
-      { id: 0, item: "활동일정", link: "#" },
-      { id: 1, item: "기술블로그", link: "#" },
-      { id: 2, item: "포트폴리오", link: "#" },
-      { id: 3, item: "로그인", link: "#" },
-    ];
-    return navItem;
-  };
-  state = {
-    navItems: this.getNavItems(),
-  };
-
   render() {
     return (
       <div className="main-nav-list">
         <ul className="main-nav-box">
-          {this.state.navItems.map((nav) => (
-            <li className="main-nav-item">
-              <a href={nav.link}>{nav.item}</a>
-            </li>
-          ))}
+          <li className="main-nav-item">
+            <a href={this.props.navItem.link}>{this.props.navItem.item}</a>
+          </li>
         </ul>
       </div>
     );
--- src/components/common/nav/Navbar.js
+++ src/components/common/nav/Navbar.js
@@ -5,6 +5,29 @@ import "../../../assets/css/common/nav/_navbar.scss";
 import temp from "../../../assets/img/temp.png";
 
 class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      navItems: [
+        {
+          item: "활동일정",
+          link: "#",
+        },
+        {
+          item: "기술블로그",
+          link: "#",
+        },
+        {
+          item: "포트폴리오",
+          link: "#",
+        },
+        {
+          item: "로그인",
+          link: "#",
+        },
+      ],
+    };
+  }
   render() {
     return (
       <div className="main-nav">
@@ -14,7 +37,9 @@ class NavBar extends React.Component {
             <img src={temp} />
           </a>
         </div>
-        <NavItem />
+        {this.state.navItems.map((navItem, i) => {
+          return <NavItem navItem={navItem} key={i} />;
+        })}
       </div>
     );
   }
