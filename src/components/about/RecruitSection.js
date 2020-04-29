fc0cd1ba5df2d4c4ede88e1672d0de1a7a116a49
minkyung <mnk918p@gmail.com>
2020-04-29 13:56:50 +0900

    [UPDATE] recruitSection with props

--- src/components/about/RecruitSection.js
+++ src/components/about/RecruitSection.js
@@ -1,11 +1,23 @@
 import React from "react";
 import ImageSection from "../common/ImgSection";
+import SectionHeader from "../common/section/SectionHeader";
 
 class RecruitSection extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "Recruit CAU-LION",
+      subtitles: ["중앙대학교 멋쟁이사자처럼은 언제 모집하나요?"]
+    };
+  }
+
   render() {
     return (
       <div>
-        Section Header가 들어갈 부분입니다.
+        <SectionHeader
+          title={this.state.title}
+          subtitles={this.state.subtitles}
+        />
         <ImageSection />
       </div>
     );
