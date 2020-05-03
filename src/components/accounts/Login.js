61e35c04b0af53a0f48717fe736704228f96b482
YooInKeun <keun0390@naver.com>
2020-05-04 02:51:16 +0900

    [ADD] accounts API method test

--- src/components/accounts/Login.js
+++ src/components/accounts/Login.js
@@ -1,14 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import API from "../../api/accountsAPI";
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { userInfo: { username: "", password: "" } };
+  }
+
+  async login() {
+    const response = await API().login({
+      username: "yik1234",
+      password: "password1234@",
+    });
+    console.log(response);
+  }
+
   render() {
     return (
       <div>
-        <div>
-          로그인
-          <Link to="/signup">회원가입</Link>
-        </div>
+        <Link to="/signup">회원가입</Link>
+        <button onClick={this.login}>로그인</button>
       </div>
     );
   }
--- src/components/accounts/SignUp.js
+++ src/components/accounts/SignUp.js
@@ -1,10 +1,30 @@
 import React from "react";
+import API from "../../api/index";
 
 class SignUp extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { userInfo: { username: "", password: "" } };
+  }
+
+  async signUp() {
+    const response = await API().signUp({
+      username: "yik1234",
+      password: "password1234@",
+    });
+    console.log(response);
+  }
+
   render() {
     return (
       <div>
         <div>회원가입 페이지 입니다.</div>
+        <div>
+          ID: <input></input>
+          PW: <input></input>
+          <button onClick={this.signUp}>회원가입</button>
+        </div>
       </div>
     );
   }
