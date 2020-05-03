e68301b85ec0b9ae03a04527b27d43cfc70995d5
YooInKeun <keun0390@naver.com>
2020-05-03 18:28:52 +0900

    [UPDATE] main page structure

--- src/pages/main/Index.js
+++ src/pages/main/Index.js
@@ -1,22 +1,20 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
-import Router from "../routes/Home";
-import Login from "../../pages/accounts/Login";
-import SignUp from "../../pages/accounts/SignUp";
-import NavBar from "../../components/common/nav/Navbar";
+import Navbar from "../../components/common/nav/Navbar";
+import ImgSection from "../../components/common/ImgSection";
+import IntroSection from "../../components/about/IntroSection";
+import ActivitySection from "../../components/about/ActivitySection";
+import RecruitSection from "../../components/about/RecruitSection";
 import Footer from "../../components/common/Footer";
 
 class Index extends React.Component {
   render() {
     return (
       <div>
-        <BrowserRouter>
-          <NavBar />
-          <Route exact path="/" component={Router} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/signup" component={SignUp} />
-        </BrowserRouter>
-
+        <Navbar />
+        <ImgSection />
+        <IntroSection />
+        <ActivitySection />
+        <RecruitSection />
         <Footer />
       </div>
     );
