0a3735a82469eb8c085ffa959913cad2b88045c0
YooInKeun <keun0390@naver.com>
2020-05-03 18:28:21 +0900

    [UPDATE] login page structure

--- src/pages/accounts/Login.js
+++ src/pages/accounts/Login.js
@@ -1,11 +1,15 @@
 import React from "react";
+import Navbar from "../../components/common/nav/Navbar";
 import Login from "../../components/accounts/Login";
+import Footer from "../../components/common/Footer";
 
 class Accounts extends React.Component {
   render() {
     return (
       <div>
+        <Navbar />
         <Login />
+        <Footer />
       </div>
     );
   }
