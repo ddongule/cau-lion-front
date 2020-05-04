a10f7280ce4ff7a231caf095656593cf8135abf4
minkyung <mnk918p@gmail.com>
2020-05-04 18:16:41 +0900

    [UPDATE] accounts input, validation

--- src/components/accounts/Login.js
+++ src/components/accounts/Login.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import API from "../../api/accountsAPI";
+import "../../assets/css/accounts/signup.scss";
 
 class Login extends React.Component {
   constructor(props) {
@@ -19,9 +20,38 @@ class Login extends React.Component {
 
   render() {
     return (
-      <div>
-        <Link to="/signup">회원가입</Link>
-        <button onClick={this.login}>로그인</button>
+      <div className="accounts-container">
+        <p className="accounts-title">로그인</p>
+        <form>
+          <div className="accounts-input-section">
+            <input
+              type="email"
+              className="accounts-input"
+              placeholder="Email"
+              autoFocus
+              required
+            />
+          </div>
+          <div className="accounts-input-section">
+            <input
+              type="password"
+              className="accounts-input"
+              placeholder="Password"
+              required
+            />
+          </div>
+          <input type="radio" required />
+          <label>Remember me?</label>
+          <Link to="#" className="accounts-password-reset">
+            비밀번호를 잊으셨나요?
+          </Link>
+          <div className="signup-have-no-accounts">
+            <p>계정이 없으신가요?</p>
+            <Link to="/signup" className="signup-or-login">
+              회원가입 하기
+            </Link>
+          </div>
+        </form>
       </div>
     );
   }
--- src/components/accounts/SignUp.js
+++ src/components/accounts/SignUp.js
@@ -1,30 +1,146 @@
 import React from "react";
-import API from "../../api/index";
+import { Link } from "react-router-dom";
+// import API from "../../api/index";
+import "../../assets/css/accounts/signup.scss";
 
 class SignUp extends React.Component {
   constructor(props) {
     super(props);
-
-    this.state = { userInfo: { username: "", password: "" } };
+    this.state = {
+      email: "",
+      password: "",
+      password_confirmation: "",
+      name: "",
+      department: "",
+      phone_number: "",
+    };
   }
-
-  async signUp() {
-    const response = await API().signUp({
-      username: "yik1234",
-      password: "password1234@",
+  handleChange = e => {
+    this.setState({
+      [e.target.name] : e.target.value
     });
-    console.log(response);
+  };
+  handlePasswordInput(passwordInput) {
+    this.setState({password:passwordInput});
   }
-
+  handlePasswordConfirmInput(confirmPasswordInput) {
+    this.setState({ confirmPassword: confirmPasswordInput});
+  }
+  doesPasswordMatch() {
+    const {password, confirmPassword} = this.state;
+    return password===confirmPassword;
+  }
+  confirmPasswords() {
+    const {confirmPassword} = this.state;
+    if (confirmPassword) {
+      return this.doesPasswordMatch() ? "is-valid" : "is-invalid";
+    }
+  }
+  renderFeedbackMessage() {
+    const {confirmPassword} = this.state;
+    if (confirmPassword) {
+      if (!this.doesPasswordMatch()) {
+        return (
+          <div className="invalid-feedback">패스워드가 일치하지 않습니다.</div>
+        )
+      }
+    }
+  }
   render() {
     return (
-      <div>
-        <div>회원가입 페이지 입니다.</div>
-        <div>
-          ID: <input></input>
-          PW: <input></input>
-          <button onClick={this.signUp}>회원가입</button>
-        </div>
+      <div className="accounts-container">
+        <p className="accounts-title">회원가입</p>
+        <form>
+          <div className="accounts-input-section">
+            <input
+              type="email"
+              name="email"
+              className="accounts-input"
+              placeholder="이메일"
+              autoFocus
+              required
+              autoComplete="off"
+              pattern="\S+"
+              onChange={this.handleChange}
+              value={this.state.email}
+            />
+          </div>
+          <div className="accounts-input-section">
+            <input
+              type="password"
+              minLength="6"
+              name="password"
+              className="accounts-input"
+              placeholder="비밀번호"
+              required
+              autoComplete="off"
+              onChange={e => this.handlePasswordInput(e.target.value)}
+              value={this.state.password}
+            />
+          </div>
+          <div className="accounts-input-section">
+            <input
+              type="password"
+              minLength="6"
+              name="password_confirmation"
+              className="accounts-input"
+              placeholder="비밀번호 확인"
+              required
+              autoComplete="off"
+              onChange={e => this.handlePasswordConfirmInput(e.target.value)}
+              value={this.state.password_confirmation}
+            />
+            {this.renderFeedbackMessage()}
+          </div>
+          <div className="accounts-input-section">
+            <input
+              type="text"
+              name="name"
+              className="accounts-input"
+              placeholder="이름"
+              required
+              autoComplete="off"
+              pattern="\S+"
+              onChange={this.handleChange}
+              value={this.state.name}
+            />
+          </div>
+          <div className="accounts-input-section">
+            <input
+              type="text"
+              name="department"
+              className="accounts-input"
+              placeholder="학과"
+              required
+              autoComplete="off"
+              pattern="\S+"
+              onChange={this.handleChange}
+              value={this.state.department}
+            />
+          </div>
+          <div className="accounts-input-section">
+            <input
+              type="tel"
+              name="phone_number"
+              className="accounts-input"
+              placeholder="전화번호(010-0000-0000)"
+              required
+              autoComplete="off"
+              pattern="^0[0-9][0-9]-[0-9]*-[0-9]*$"
+              onChange={this.handleChange}
+              value={this.state.phone_number}
+            />
+          </div>
+          <button onClick={this.signUp} className="accounts-btn">
+            회원가입
+          </button>
+          <div className="signup-have-accounts">
+            <p>계정이 있으신가요?</p>
+            <Link to="/login" className="signup-or-login">
+              로그인 하기
+            </Link>
+          </div>
+        </form>
       </div>
     );
   }
