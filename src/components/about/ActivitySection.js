5509b005c39942d95b41c995ab72b8cc190123d9
YooInKeun <keun0390@naver.com>
2020-05-05 11:34:23 +0900

    [ADD] activity section images

--- src/components/about/ActivitySection.js
+++ src/components/about/ActivitySection.js
@@ -11,34 +11,34 @@ class Activity extends React.Component {
       subtitles: ["중앙대학교 멋쟁이 사자처럼은 어떤 활동들을 하나요?"],
       cards: [
         {
-          img: "중앙대 세션 사진",
+          imgSrc: "props_img/regular-session.jpg",
           category: "정기 세션 진행",
           content: "주 1회 혹은 2회씩 정기 세션을 진행합니다",
         },
         {
-          img: "해커톤 사진",
+          imgSrc: "props_img/cau-hackathon.jpg",
           category: "중앙대 멋사 해커톤",
           content:
             "자체 해커톤을 통해 실제 서비스를 구현하고 협업을 경험해봅니다",
         },
         {
-          img: "회식 사진",
+          imgSrc: "props_img/friendship-meeting.jpg",
           category: "친목 모임",
           content: "멋쟁이 사자처럼 동아리원들과의 친목을 다집니다",
         },
         {
-          img: "연합 세션 사진",
+          imgSrc: "props_img/alliance-session.jpg",
           category: "연합 세션 진행",
           content: "다른 학교와의 연합 세션을 통해 통찰력을 높입니다",
         },
         {
-          img: "전체 해커톤 사진",
+          imgSrc: "props_img/all-hackathon.jpg",
           category: "전체 해커톤",
           content:
             "전체 해커톤을 통해 전국 멋사인들과 아이디어 및 서비스를 공유합니다",
         },
         {
-          img: "엠티 사진",
+          imgSrc: "엠티 사진",
           category: "중앙대 멋사 MT",
           content: "MT를 통해 동아리원들과의 즐거운 추억을 쌓습니다",
         },
--- src/components/common/Card.js
+++ src/components/common/Card.js
@@ -3,8 +3,13 @@ import "../../assets/css/common/card.scss";
 
 class Card extends React.Component {
   render() {
+    const imgStyle = {
+      backgroundImage: `url(${this.props.card.imgSrc})`,
+      backgroundSize: "contain"
+    };
+
     return (
-      <div className="card">
+      <div className="card" style={imgStyle}>
         <h1 className="card-content">{this.props.card.content}</h1>
         <h1 className="card-category">{this.props.card.category}</h1>
       </div>
