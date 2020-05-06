5b94ac3f0bbc469c203da7e06463019df4b9be33
minkyung <mnk918p@gmail.com>
2020-05-06 13:12:04 +0900

    [UPDATE] signup validation

--- src/components/accounts/SignUp.js
+++ src/components/accounts/SignUp.js
@@ -9,13 +9,55 @@ class SignUp extends React.Component {
     super(props);
     this.state = {
       email: "",
+      isEmailValid: false,
       password: "",
-      password_confirmation: "",
+      confirmPassword: "",
       name: "",
+      isNameValid: false,
       department: "",
-      phone_number: "",
+      isDepartmentValid: false,
+      phoneNumber: "",
+      isPhoneNumberValid: false,
     };
   }
+  inputClassNameHelper = boolean => {
+    switch (boolean) {
+      case true:
+        return 'is-valid';
+      case false:
+        return 'is-invalid';
+      default:
+        return '';
+    }
+  };
+
+  isEveryFieldValid = () => {
+    const { isEmailValid, isNameValid, isDepartmentValid, isPhoneNumberValid } = this.state;
+    return isEmailValid && isNameValid && isDepartmentValid && isPhoneNumberValid;
+  }
+  renderSubmitBtn = () => {
+    if (this.isEveryFieldValid()) {
+      return (
+        <button
+          onClick={() => this.signUp(this.state)}
+          className="accounts-btn"
+        >
+          회원가입
+        </button>
+      )
+    } 
+  
+    return (
+      <button
+        onClick={() => this.signUp(this.state)}
+        className="accounts-invalid-btn"
+        disabled
+      >
+        회원가입
+      </button>
+    )
+  }
+
   handleChange = (e) => {
     this.setState({
       [e.target.name]: e.target.value,
@@ -31,7 +73,7 @@ class SignUp extends React.Component {
     const { password, confirmPassword } = this.state;
     return password === confirmPassword;
   }
-  confirmPasswords() {
+  confirmPasswordClassName() {
     const { confirmPassword } = this.state;
     if (confirmPassword) {
       return this.doesPasswordMatch() ? "is-valid" : "is-invalid";
@@ -47,6 +89,90 @@ class SignUp extends React.Component {
       }
     }
   }
+  validateName = name => {
+    if (name.length > 1) {
+      this.setState({
+        isNameValid: true,
+        name
+      });
+    } else {
+      this.setState({
+        isNameValid: false,
+        name
+      });
+    }
+  };
+
+  isEnteredNameValid = () => {
+    const { name, isNameValid } = this.state;
+    if (name) return isNameValid;
+  };
+
+  validateDepartment = department => {
+    const departmentRegExp = /^[가-힣]+학과$/;
+    if (department.match(departmentRegExp)) {
+      this.setState({
+        isDepartmentValid: true,
+        department
+      });
+    } else {
+      this.setState({
+        isDepartmentValid: false,
+        department
+      });
+    }
+  };
+
+  isEnteredDepartmentValid = () => {
+    const { department, isDepartmentValid } = this.state;
+    if (department) return isDepartmentValid;
+  };
+
+
+  validateEmail = email => {
+    const emailRegExp = /^[\w-]+(\.[\w-]+)*@([a-z0-9-]+(\.[a-z0-9-]+)*?\.[a-z]{2,6}|(\d{1,3}\.){3}\d{1,3})(:\d{4})?$/;
+
+    if (email.match(emailRegExp)) {
+      this.setState({
+        isEmailValid: true,
+        email
+      });
+    } else {
+      this.setState({
+        isEmailValid: false,
+        email
+      });
+    }
+  };
+
+  isEnteredEmailValid = () => {
+    const { email, isEmailValid } = this.state;
+
+    if (email) return isEmailValid;
+  };
+
+  validatePhoneNumber = phoneNumberInput => {
+    const phoneNumberRegExp = /^\d{3}-\d{3,4}-\d{4}$/;
+
+    if (phoneNumberInput.match(phoneNumberRegExp)) {
+      this.setState({
+        isPhoneNumberValid: true,
+        phoneNumber: phoneNumberInput
+      });
+    } else {
+      this.setState({
+        isPhoneNumberValid: false,
+        phoneNumber: phoneNumberInput
+      });
+    }
+  };
+
+  isEnteredPhoneNumberValid = () => {
+    const { phoneNumber, isPhoneNumberValid } = this.state;
+
+    if (phoneNumber) return isPhoneNumberValid;
+  };
+
   async signUp(userInfo) {
     const response = await API().signUp(userInfo);
     console.log(response);
@@ -61,13 +187,12 @@ class SignUp extends React.Component {
             <input
               type="email"
               name="email"
-              className="accounts-input"
+              className={`accounts-input ${this.inputClassNameHelper(this.isEnteredEmailValid())}`}
               placeholder="이메일"
               autoFocus
               required
               autoComplete="off"
-              pattern="\S+"
-              onChange={this.handleChange}
+              onChange={e => this.validateEmail(e.target.value)}
               value={this.state.email}
             />
           </div>
@@ -80,7 +205,7 @@ class SignUp extends React.Component {
               placeholder="비밀번호"
               required
               autoComplete="off"
-              onChange={this.handleChange}
+              onChange={e => this.handlePasswordInput(e.target.value)}
               value={this.state.password}
             />
           </div>
@@ -89,11 +214,11 @@ class SignUp extends React.Component {
               type="password"
               minLength="6"
               name="password_confirmation"
-              className="accounts-input"
+              className={`accounts-input ${this.confirmPasswordClassName()}`}
               placeholder="비밀번호 확인"
               required
               autoComplete="off"
-              onChange={this.handleChange}
+              onChange={e => this.handlePasswordConfirmInput(e.target.value)}
               value={this.state.password_confirmation}
             />
             {this.renderFeedbackMessage()}
@@ -102,12 +227,11 @@ class SignUp extends React.Component {
             <input
               type="text"
               name="name"
-              className="accounts-input"
+              className={`accounts-input ${this.inputClassNameHelper(this.isEnteredNameValid())}`}
               placeholder="이름"
               required
               autoComplete="off"
-              pattern="\S+"
-              onChange={this.handleChange}
+              onChange={e => this.validateName(e.target.value)}
               value={this.state.name}
             />
           </div>
@@ -115,34 +239,27 @@ class SignUp extends React.Component {
             <input
               type="text"
               name="department"
-              className="accounts-input"
-              placeholder="학과"
+              className={`accounts-input ${this.inputClassNameHelper(this.isEnteredDepartmentValid())}`}
+              placeholder="XXXX학과"
               required
               autoComplete="off"
-              pattern="\S+"
-              onChange={this.handleChange}
+              onChange={e => this.validateDepartment(e.target.value)}
               value={this.state.department}
             />
           </div>
           <div className="accounts-input-section">
             <input
               type="tel"
-              name="phone_number"
-              className="accounts-input"
+              name="phoneNumber"
+              className={`accounts-input ${this.inputClassNameHelper(this.isEnteredPhoneNumberValid())}`}
               placeholder="전화번호(010-0000-0000)"
               required
               autoComplete="off"
-              pattern="^0[0-9][0-9]-[0-9]*-[0-9]*$"
-              onChange={this.handleChange}
-              value={this.state.phone_number}
+              onChange={e => this.validatePhoneNumber(e.target.value)}
+              value={this.state.phoneNumber}
             />
           </div>
-          <button
-            onClick={() => this.signUp(this.state)}
-            className="accounts-btn"
-          >
-            회원가입
-          </button>
+          {this.renderSubmitBtn()}
           <div className="signup-have-accounts">
             <p>계정이 있으신가요?</p>
             <Link to="/login" className="signup-or-login">
