0b8a8096d64c3e09a9a23eb6baed054cc52be8a6
YooInKeun <keun0390@naver.com>
2020-05-18 20:21:37 +0900

    [ADD] comma in RecruitSection state

--- src/components/about/RecruitSection.js
+++ src/components/about/RecruitSection.js
@@ -7,7 +7,7 @@ class RecruitSection extends React.Component {
     super(props);
     this.state = {
       title: "Recruit CAU-LION",
-      subtitles: ["중앙대학교 멋쟁이사자처럼은 언제 모집하나요?"]
+      subtitles: ["중앙대학교 멋쟁이사자처럼은 언제 모집하나요?"],
     };
   }
 
