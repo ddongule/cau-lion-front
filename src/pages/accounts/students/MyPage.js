660972de350f16466dbb32b214d2bbc152b2fb0d
minkyung <mnk918p@gmail.com>
2020-05-28 17:57:07 +0900

    [UPDATE] mypage divs

--- src/pages/accounts/students/MyPage.js
+++ src/pages/accounts/students/MyPage.js
@@ -2,17 +2,22 @@ import React from "react";
 import Layout from "../../../components/common/layout/Layout";
 import SideNavbar from "../../../components/common/nav/SideNavbar";
 import MyBoard from "../../../components/accounts/MyBoard";
+import MyInfo from "../../../components/accounts/MyInfo";
 import "../../../assets/css/accounts/students/my-page.scss";
 
 class Accounts extends React.Component {
   render() {
     return (
-      <div>
-        <Layout>
+      <Layout>
+        <div className="mypage-container">
           <SideNavbar />
-          <MyBoard />
-        </Layout>
-      </div>
+          <div className="mypage-contents">
+            <div className="mypage-title">마이페이지</div>
+            <MyInfo />
+            <MyBoard />
+          </div>
+        </div>
+      </Layout>
     );
   }
 }
