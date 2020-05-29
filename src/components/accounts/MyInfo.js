e7adff77916aee257c7929fc416ecd290e4d383a
ddongule <mnk918p@gmail.com>
2020-05-29 10:27:54 +0900

    [UPDATE] myinfo card

--- src/components/accounts/MyInfo.js
+++ src/components/accounts/MyInfo.js
@@ -6,13 +6,18 @@ class MyInfo extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      school: "중앙대학교",
-      clubNum: "8기",
-      department: "중앙멋사학과",
-      name: "김중앙",
+      // API로 가져와야 함
+      infos: [
+        {
+          school: "중앙대학교",
+          clubNum: "8기",
+          department: "중앙멋사학과",
+          name: "김중앙",
+        },
+      ],
       cards: [
         {
-          myImgUrl: "",
+          img: "",
         },
       ],
     };
@@ -21,14 +26,22 @@ class MyInfo extends React.Component {
     return (
       <div className="mypage-myinfo">
         <div className="myinfo-title">나의 정보</div>
-        {this.state.cards.map((card, i) => {
-          return <Card card={card} key={i} />;
-        })}
         <div className="myinfo-contents">
-          {this.state.school}
-          {this.state.clubNum}
-          {this.state.department}
-          {this.state.name}
+          {this.state.cards.map((card, i) => {
+            return <Card card={card} key={i} />;
+          })}
+          <div className="myinfo-contents">
+            {this.state.infos.map((info, i) => {
+              return (
+                <div>
+                  {info.school}
+                  {info.clubNum}
+                  {info.department}
+                  {info.name}
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
     );
