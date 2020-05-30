6469c9f916e9fa0d870948b90a58801fa6cfc131
ddongule <mnk918p@gmail.com>
2020-05-30 10:15:22 +0900

    [ADD] my assignment

---
+++ src/components/accounts/MyAssignment.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Card from "../common/Card";
+import "../../assets/css/accounts/students/my-assignment.scss";
+
+class MyAssignment extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cards: [
+        {
+          thumbnail: "",
+        },
+        {
+          thumbnail: "",
+        },
+        {
+          thumbnail: "",
+        },
+        {
+          thumbnail: "",
+        },
+        {
+          thumbnail: "",
+        },
+      ],
+    };
+  }
+  render() {
+    return (
+      <div>
+        나의 과제 페이지 임다
+        {this.state.cards.map((card, i) => {
+          return <Card card={card} key={i} />;
+        })}
+      </div>
+    );
+  }
+}
+export default MyAssignment;
